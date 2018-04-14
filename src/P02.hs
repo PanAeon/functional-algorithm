94c5e3fd11fa5a025199f42f0e68bb8639db728e
PanAeon <vitalii.voloshyn@gmail.com>
2018-04-14 20:26:10 +0100

    To be honest, I do not quite remember everything I changed here today. But it is all good, I tell ya.

--- src/P02.hs
+++ src/P02.hs
@@ -25,3 +25,17 @@ scount z zs = length $ filter (>z) zs
 
 tails' [] = []
 tails' (x:xs) = (x:xs) : tails xs
+
+-- FIXME: revisit some time
+table [x] = [(x,0)]
+table xs = join' (m-n) (table ys) (table zs)
+  where
+    m = length xs
+    n = m `div` 2
+    (ys, zs) = splitAt n xs
+
+join' 0 txs [] = txs
+join' n [] tys = tys
+join' n txs@((x,c):txs') tys@((y,d): tys')
+   | x < y  = (x, c + n) : join' n txs' tys
+   | x >= y = (y,d) : join' (n-1) txs tys'

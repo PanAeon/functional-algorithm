b1398b39581e93a1259e0f05f4715e1acd919076
PanAeon <vitalii.voloshyn@gmail.com>
2018-06-27 22:41:46 +0100

    Handled a particular error.

--- src/BreadthFirstTraversals.hs
+++ src/BreadthFirstTraversals.hs
@@ -20,9 +20,20 @@ ex1 = [Node 1 [
         Node 4 []
       ]]
 
+ex2 = [Node 1488 [Node 1711 []], Node 1499 []]
+
+{-
+#!/usr/bin/env bash
+
+COMMIT_MESSAGE=$(curl -s whatthecommit.com |  grep -A2 '<div id="content">' |  tail -n +2 | head -n +1 | sed 's/<p>//')
+git commit -m "$COMMIT_MESSAGE"
+
+
+-}
+
 -- FIXME: rewrite with cont/zippers?
-breadthFirst' :: Forest a -> [a]
-breadthFirst' fs = (foldr f z0 fs) []
+breadthFirst' :: Forest a -> Forest a -> [a]
+breadthFirst' fs z = (foldr f z0 fs) [z]
 
 f :: Tree a -> ([Forest a] -> [a]) -> ([Forest a] -> [a])
 f (Node x xs) cont = \fsts -> x : (cont (xs : fsts) )
@@ -76,7 +87,7 @@ breadthFirst :: Forest a -> [a]
 breadthFirst ts = foldr f b ts []
   where
     f :: Tree a -> ([Forest a] -> [a]) -> ([Forest a] -> [a])
-    f (Node x xs) fw = \prev -> x : fw (xs : prev)
+    f (Node x children) fronts = \siblings -> x : fronts (children : siblings)
 
     b :: [Forest a] -> [a]
     b [] = []

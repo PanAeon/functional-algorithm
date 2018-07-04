ea97f8f9072bdeb9d2f0c4b163376481de57c947
PanAeon <vitalii.voloshyn@gmail.com>
2018-07-04 23:37:00 +0100

    work in progress

--- src/LCS.hs
+++ src/LCS.hs
@@ -1,8 +1,13 @@
 module LCS
-    (lcsPlain
+    (lcsPlain,
+     lcsSlow,
+     lcsCached
     ) where
 
 import           Data.List
+import           Data.Vector(Vector)
+import   qualified Data.Vector as V
+import Debug.Trace
 
 -- Write bloody test or whatever
 
@@ -15,15 +20,69 @@ lcsPlain xs [] = []
 lcsPlain (x:xs) ys =  case dropWhile (/=x) ys of
                            []       -> lcsPlain xs ys
                            (_:ys'') -> max' (lcsPlain xs ys) (x: lcsPlain xs ys'')
-  where
-    max' xs ys = if length xs > length ys then xs else ys --hmm, length should be cached
+
+max' xs ys = if length xs > length ys then xs else ys --hmm, length should be cached
     -- continuousSubsequenceLength xs ys = length $ takeWhile (uncurry (==)) $ zip xs ys
     -- subsequenceLength xs ys = undefined
       -- do
       --   tx <- tails xs
       --   ty <- tails ys
-
+lcsSlow :: Ord a => [a] -> [a] -> [a]
+lcsSlow [] ys = []
+lcsSlow xs [] = []
+lcsSlow (x:xs) (y:ys)
+  | x == y    = x : lcsSlow xs ys
+  | otherwise = max' (lcsSlow (x:xs) ys) (lcsSlow xs (y:ys))
 --------------------------------------------------------------------------------
+
+-- FIXME: lcsCached "GXTXAB" "GGTAB"
+-- lcsCached :: Ord a => [a] -> [a] -> [a]
+lcsCached xs ys = if (fst . V.head . fst) res > (fst . V.head . snd ) res
+                  then (snd . V.head . fst) res
+                  else (snd . V.head . snd) res
+  where
+    n = length xs
+    m = length ys
+    n' = min n m
+    -- xsr = reverse xs
+    -- ysr = reverse ys
+    xv = V.fromList xs
+    yv = V.fromList ys
+    (xs0, ys0) = (V.replicate (n+1) (0,[]), V.replicate (m+1) (0,[]))
+    res = foldl f (xs0, ys0) [1..n']
+    f (row,col) i = traceShowId $ (V.fromList $ row', V.fromList $  col')
+      where
+        xidx = n - i
+        yidx = m - i
+        xi = xv V.! xidx
+        yi = yv V.! yidx
+        -- FIXME: refactor this shit, and check this fucking reverse
+        row' = tail $ reverse $  scanr fx (col V.! yidx) [0 .. xidx]
+
+        col' = init $ scanr fy (row V.! xidx) [0 .. yidx]
+
+        fx l right =
+          let
+            bottomRight = row V.! (l + 1)
+            bottom = row V.! l
+          in if xv V.! l == yi
+                 then (1 + fst bottomRight, yi : (snd bottomRight))
+                 else if fst right > fst bottom
+                      then right
+                      else bottom
+
+        fy l bottom =
+          let
+            bottomRight = col V.! (l + 1)
+            right = col V.! l
+          in if yv V.! l == xi
+                 then (1 + fst bottomRight, xi : (snd bottomRight))
+                 else if fst bottom > fst right
+                      then bottom
+                      else right
+
+
+
 
 -- good try, but no
 -- foo xs ys = foldr f [] (tails xs)
--- test/LCSTest.hs
+++ test/LCSTest.hs
@@ -6,18 +6,44 @@ import           Test.Tasty.Hspec
 
 
 
-spec_lcsPlain :: Spec
-spec_lcsPlain =
+spec_lcs :: Spec
+spec_lcs = do
   describe "LCS Plain" $ do
-  it "should satisfy simple test" $ do
-    let (xs,ys) = ([11, 1,12,2,13,3,16], [0,1,7,8,2,7,9,3])
-    lcsPlain xs ys `shouldBe`  [1,2,3]
-  it "should satisfy example test" $ do
-    let (xs, ys) = ("ABCDGH", "AEDFHR")
-    lcsPlain xs ys `shouldBe` "ADH"
-  it "should satisfy second example" $ do
-    let (xs, ys) = ("AGGTAB", "GXTXAYB")
-    lcsPlain xs ys `shouldBe` "GTAB"
-  it "should satisfy reverse second example" $ do
-   let (xs, ys) = ("GXTXAYB", "AGGTAB")
-   lcsPlain xs ys `shouldBe` "GTAB"
+    it "should satisfy simple test" $ do
+      let (xs,ys) = ([11, 1,12,2,13,3,16], [0,1,7,8,2,7,9,3])
+      lcsPlain xs ys `shouldBe`  [1,2,3]
+    it "should satisfy example test" $ do
+      let (xs, ys) = ("ABCDGH", "AEDFHR")
+      lcsPlain xs ys `shouldBe` "ADH"
+    it "should satisfy second example" $ do
+      let (xs, ys) = ("AGGTAB", "GXTXAYB")
+      lcsPlain xs ys `shouldBe` "GTAB"
+    it "should satisfy reverse second example" $ do
+     let (xs, ys) = ("GXTXAYB", "AGGTAB")
+     lcsPlain xs ys `shouldBe` "GTAB"
+  describe "LCS slow" $ do
+    it "should satisfy simple test" $ do
+      let (xs,ys) = ([11, 1,12,2,13,3,16], [0,1,7,8,2,7,9,3])
+      lcsSlow xs ys `shouldBe`  [1,2,3]
+    it "should satisfy example test" $ do
+      let (xs, ys) = ("ABCDGH", "AEDFHR")
+      lcsSlow xs ys `shouldBe` "ADH"
+    it "should satisfy second example" $ do
+      let (xs, ys) = ("AGGTAB", "GXTXAYB")
+      lcsSlow xs ys `shouldBe` "GTAB"
+    it "should satisfy reverse second example" $ do
+     let (xs, ys) = ("GXTXAYB", "AGGTAB")
+     lcsSlow xs ys `shouldBe` "GTAB"
+  describe "LCS cached" $ do
+    it "should satisfy simple test" $ do
+      let (xs,ys) = ([11, 1,12,2,13,3,16], [0,1,7,8,2,7,9,3])
+      lcsCached xs ys `shouldBe`  [1,2,3]
+    it "should satisfy example test" $ do
+      let (xs, ys) = ("ABCDGH", "AEDFHR")
+      lcsCached xs ys `shouldBe` "ADH"
+    it "should satisfy second example" $ do
+      let (xs, ys) = ("AGGTAB", "GXTXAYB")
+      lcsCached xs ys `shouldBe` "GTAB"
+    it "should satisfy reverse second example" $ do
+     let (xs, ys) = ("GXTXAYB", "AGGTAB")
+     lcsCached xs ys `shouldBe` "GTAB"
